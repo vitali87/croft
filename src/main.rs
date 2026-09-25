f095d7f0df83d8a84bcd9a5f16d85e74b41cbaee
vitali87 <eheva87@gmail.com>
2026-09-25 02:36:33 +0100

    chore: release 0.1.989

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,8 @@ mod review_threads;
 mod rewind;
 mod sarif;
 mod scrubber;
+// Used by the Search Editor commands, which land in the next change.
+#[allow(dead_code)]
 mod search_editor;
 mod session;
 mod session_host;
